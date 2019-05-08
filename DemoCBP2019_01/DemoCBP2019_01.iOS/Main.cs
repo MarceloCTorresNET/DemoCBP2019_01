ff6818b5da849a1bac5716d7f1f783282d687da4
Marcelo Cesar Torres <mtorres@esri.co>
2019-05-07 21:14:43 -0500

    Agregar archivos de proyecto.

---
+++ DemoCBP2019_01/DemoCBP2019_01.iOS/Main.cs
@@ -0,0 +1,12 @@
+﻿using UIKit;
+
+namespace DemoCBP2019_01.iOS
+{
+  public class Application
+  {
+    static void Main(string[] args)
+    {
+      UIApplication.Main(args, null, "AppDelegate");
+    }
+  }
+}
---
+++ DemoCBP2019_01/DemoCBP2019_01/MapPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace DemoCBP2019_01
+{
+  public partial class MapPage : ContentPage
+  {
+    public MapPage()
+    {
+      InitializeComponent();
+    }
+
+    // Map initialization logic is contained in MapViewModel.cs
+  }
+}
