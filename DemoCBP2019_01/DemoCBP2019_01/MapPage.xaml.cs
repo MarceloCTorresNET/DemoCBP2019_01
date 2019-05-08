a98e326f8ffc925944d74399dc6020b6e59fae66
Marcelo Cesar Torres <mtorres@esri.co>
2019-05-07 22:12:07 -0500

    Demo 1 CBP 2019

--- DemoCBP2019_01/DemoCBP2019_01/MapPage.xaml.cs
+++ DemoCBP2019_01/DemoCBP2019_01/MapPage.xaml.cs
@@ -1,4 +1,17 @@
-﻿using Xamarin.Forms;
+﻿using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Symbology;
+using Esri.ArcGISRuntime.UI;
+using Esri.ArcGISRuntime.Xamarin.Forms;
+using System.Linq;
+using Xamarin.Forms;
+
+#if WINDOWS_UWP
+using Colors = Windows.UI.Colors;
+#else
+using Colors = System.Drawing.Color;
+#endif
+
 
 namespace DemoCBP2019_01
 {
@@ -10,5 +23,43 @@ namespace DemoCBP2019_01
     }
 
     // Map initialization logic is contained in MapViewModel.cs
+
+    private void BotonCentrar_Clicked(object sender, System.EventArgs e)
+    {
+      // TODO 8. Centrar Mapa
+      //var mapPoint = new MapPoint(-74.051331, 4.673646, SpatialReferences.Wgs84);
+      //await Mapa.SetViewpointAsync(new Viewpoint(mapPoint, 5000));
+    }
+
+    private void Mapa_GeoViewTapped(object sender, GeoViewInputEventArgs e)
+    {
+      // TODO 7. Dibujar Eventos en el Mapa
+      //var graphic = new Graphic()
+      //{
+      //  Geometry = e.Location,
+      //  Symbol = new SimpleMarkerSymbol
+      //  {
+      //    Color = Colors.Red,
+      //    Style = SimpleMarkerSymbolStyle.Diamond,
+      //    Size = 20
+      //  }
+      //};
+      //var graphicOverlay = Mapa.GraphicsOverlays.Where(go => go.Id == "Eventos").FirstOrDefault();
+      //if(graphicOverlay != null)
+      //{
+      //  graphicOverlay.Graphics.Add(graphic);
+      //}
+    }
+
+    private void BotonBorrar_Clicked(object sender, System.EventArgs e)
+    {
+      // TODO 9. Borrar eventos
+      //var graphicOverlay = Mapa.GraphicsOverlays.Where(go => go.Id == "Eventos").FirstOrDefault();
+      //if (graphicOverlay != null)
+      //{
+      //  graphicOverlay.Graphics.Clear();
+      //}
+
+    }
   }
 }
--- DemoCBP2019_01/DemoCBP2019_01/MapViewModel.cs
+++ DemoCBP2019_01/DemoCBP2019_01/MapViewModel.cs
@@ -14,6 +14,14 @@ using Esri.ArcGISRuntime.Symbology;
 using Esri.ArcGISRuntime.Tasks;
 using Esri.ArcGISRuntime.UI;
 
+// TODO 3. Agregar alias a las Bibliotecas de Colores
+//#if WINDOWS_UWP
+//using Colors = Windows.UI.Colors;
+//#else
+//using Colors = System.Drawing.Color;
+//#endif
+
+
 namespace DemoCBP2019_01
 {
   /// <summary>
@@ -23,6 +31,27 @@ namespace DemoCBP2019_01
   {
     public MapViewModel()
     {
+      //TODO: 3. Agregar Código Constructor: 
+      //GraphicsOverlayCollection = new GraphicsOverlayCollection()
+      //{
+      //  new GraphicsOverlay() { Id = "Esri Colombia"},
+      //  new GraphicsOverlay() { Id = "Eventos"}
+      //};
+
+      //var mapPoint = new MapPoint(-74.051331, 4.673646, SpatialReferences.Wgs84);
+
+      //var graphic = new Graphic()
+      //{
+      //  Geometry = mapPoint,
+      //  Symbol = new SimpleMarkerSymbol
+      //  {
+      //    Color = Colors.Green,
+      //    Style = SimpleMarkerSymbolStyle.Circle,
+      //    Size = 10
+      //  }
+      //};
+      //GraphicsOverlayCollection.First().Graphics.Add(graphic);
+      //Map.InitialViewpoint = new Viewpoint(mapPoint, 5000);
 
     }
 
@@ -36,6 +65,15 @@ namespace DemoCBP2019_01
       get { return _map; }
       set { _map = value; OnPropertyChanged(); }
     }
+
+    // TODO: 1. Agregar CapasGraficas y un gráfico
+    //private GraphicsOverlayCollection _graphicOverlyaCollection;
+
+    //public GraphicsOverlayCollection GraphicsOverlayCollection
+    //{
+    //  get { return _graphicOverlyaCollection; }
+    //  set { _graphicOverlyaCollection = value; OnPropertyChanged(); }
+    //}
 
     /// <summary>
     /// Raises the <see cref="MapViewModel.PropertyChanged" /> event
